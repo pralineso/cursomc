5b3c203c6823fd5f85d682d980dced03438eca95
Mariana <mxpixel@yahoo.com.br>
2020-02-06 17:17:48 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/pralineso/cursomc/domain/Produto.java
+++ src/main/java/com/pralineso/cursomc/domain/Produto.java
@@ -13,6 +13,8 @@ import javax.persistence.JoinColumns;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -25,6 +27,7 @@ public class Produto implements Serializable{
 	
 
 //pra tabela de realcao mutios pra muitos
+	@JsonBackReference
 	@ManyToMany
 	 @JoinTable(name = "PRODUTO_CATEGORIA",
 	 joinColumns = @JoinColumn(name = "produto_id"),

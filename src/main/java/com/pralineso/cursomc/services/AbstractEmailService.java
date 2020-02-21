152a93f1cfb1647d7ef0b79e3ac3935d722785db
Mariana <mxpixel@yahoo.com.br>
2020-02-21 14:26:55 -0300

    Esqueci a senha

--- src/main/java/com/pralineso/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/pralineso/cursomc/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.pralineso.cursomc.domain.Cliente;
 import com.pralineso.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
@@ -57,7 +58,7 @@ public abstract class AbstractEmailService implements EmailService {
 			sendOrderConfirmationEmail(obj);
 		}
 	}
-
+	
 	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
@@ -68,4 +69,21 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
 	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: "+ newPass);
+		return sm;
+	}
+
+
 }
--- src/main/java/com/pralineso/cursomc/services/EmailService.java
+++ src/main/java/com/pralineso/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.pralineso.cursomc.domain.Cliente;
 import com.pralineso.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -16,5 +17,7 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 
 }

0a15d835a5b32080211e84cfa14a95ec6bc52ed1
Mariana <mxpixel@yahoo.com.br>
2020-02-23 11:31:35 -0300

    Enviando imagem via endpoint

--- src/main/java/com/pralineso/cursomc/resources/ClienteResource.java
+++ src/main/java/com/pralineso/cursomc/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pralineso.cursomc.domain.Cliente;
@@ -24,69 +25,74 @@ import com.pralineso.cursomc.dto.ClienteNewDTO;
 import com.pralineso.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
 
-	@Autowired //instacia automaticamente
+	@Autowired // instacia automaticamente
 	private ClienteService service;
 
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 
 		Cliente obj = service.find(id);
-		return ResponseEntity.ok().body(obj);//pega o q vir no ?? e joga como corpo da pagina
+		return ResponseEntity.ok().body(obj);// pega o q vir no ?? e joga como corpo da pagina
 	}
 
-
-	//metodo pra inserir cliente(ja vai com as entidades realcionadas|telefone/endereco )
-	@RequestMapping(method=RequestMethod.POST )
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){ 
+	// metodo pra inserir cliente(ja vai com as entidades
+	// realcionadas|telefone/endereco )
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
 		Cliente obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
-		//pega uri do novo recurso inserido
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); 
+		// pega uri do novo recurso inserido
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
-
 	}
 
-	//metodo para atualizar
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+	// metodo para atualizar
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
 		Cliente obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 
-	 @PreAuthorize("hasAnyRole('ADMIN')")
-	//metodo para deletar
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	// metodo para deletar
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
-	 @PreAuthorize("hasAnyRole('ADMIN')")
-	//metodo para listar
-	@RequestMapping(method=RequestMethod.GET)
-	//lista de categoriadto pra retorna so a "parte principal da categoria"(sem os produtos vinculados)
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	// metodo para listar
+	@RequestMapping(method = RequestMethod.GET)
+	// lista de categoriadto pra retorna so a "parte principal da categoria"(sem os
+	// produtos vinculados)
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> list = service.findAll();
-		//converte a list pra dto
+		// converte a list pra dto
 		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	 @PreAuthorize("hasAnyRole('ADMIN')")
-	//listar por pagina
-	@RequestMapping(value="/page", method=RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(
-			@RequestParam(value="page", defaultValue = "0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue = "24")Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue = "nome")String orderBy, 
-			@RequestParam(value="direction", defaultValue = "ASC")String direction) {
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	// listar por pagina
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/picture", method = RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name = "file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 }

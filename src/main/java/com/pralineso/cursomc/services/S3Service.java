468d585f111fe57cdce8e86a13975c847508efcf
Mariana <mxpixel@yahoo.com.br>
2020-02-23 17:16:45 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/pralineso/cursomc/services/S3Service.java
+++ src/main/java/com/pralineso/cursomc/services/S3Service.java
@@ -1,12 +1,9 @@
 package com.pralineso.cursomc.services;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-
-import javax.management.RuntimeErrorException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.pralineso.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -40,7 +35,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 
 	}
@@ -54,7 +49,7 @@ public class S3Service {
 			LOG.info("Upload finalizado");
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao convrter URL para URI");
+			throw new FileException("Erro ao convrter URL para URI");
 		}
 	}
 

f6b4908c8f009f8674bc9aa7b70cc79aba1b26aa
Mariana <mxpixel@yahoo.com.br>
2020-02-25 12:57:01 -0300

    Padronizando formato das excecoes

--- src/main/java/com/pralineso/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/pralineso/cursomc/resources/exceptions/StandardError.java
@@ -2,40 +2,79 @@ package com.pralineso.cursomc.resources.exceptions;
 
 public class StandardError {
 	
-	private Integer status;
-	private String msg;
 	private Long timeStamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
 	
-	public StandardError(Integer status, String msg, Long timeStamp) {
+	
+	public StandardError(Long timeStamp, Integer status, String error, String message, String path) {
 		super();
-		this.status = status;
-		this.msg = msg;
 		this.timeStamp = timeStamp;
-	}
-
-	public Integer getStatus() {
-		return status;
-	}
-
-	public void setStatus(Integer status) {
 		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
 	}
 
-	public String getMsg() {
-		return msg;
-	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
 
 	public Long getTimeStamp() {
 		return timeStamp;
 	}
 
+
 	public void setTimeStamp(Long timeStamp) {
 		this.timeStamp = timeStamp;
 	}
+
+
+	public Integer getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+
+	public String getError() {
+		return error;
+	}
+
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public String getPath() {
+		return path;
+	}
+
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+	
+	
+
+	
+
 	
 	
 
